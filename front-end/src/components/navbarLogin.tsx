800ad0484422d939896e279eb34c2fac462065df
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2022-11-24 14:49:44 -0300

    Update navbarLogin.tsx

--- front-end/src/components/navbarLogin.tsx
+++ front-end/src/components/navbarLogin.tsx
@@ -1,9 +1,22 @@
 import { Fragment } from "react";
-import Footer from "./footer";
+import{ImHome} from "react-icons/im"
+
 export default function NavbarLogin() {
   return(
   <Fragment>
-  <h1> sample text</h1>
+   <nav className="navbar" role="navigation" aria-label="main navigation">
+  <div className="navbar-brand">
+  
+   <a href= {`/`} className="navbar-item is-primary" onClick={()=> refreshPage()}>
+          <ImHome/>
+    </a>
+</div>
+  
+</nav>
   </Fragment>
 )
+}
+
+function refreshPage(): void {
+  window.location.reload()
 }
